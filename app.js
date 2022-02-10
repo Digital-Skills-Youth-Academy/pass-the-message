5be6757187358a6a2667fbb0ad289ba9cfd979ec
Kayla Golder <golderkayla@gmail.com>
2022-02-10 18:55:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 
+const form = document.getElementById('message-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const messageInput = document.getElementById('message');
+    const messageRecieved = document.getElementById('passed-message');
+
+    messageRecieved.textContent = messageInput.value;
+    messageInput.value = ''
+})
